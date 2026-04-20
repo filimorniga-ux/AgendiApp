5d60508e5d6306a490d7cc65644b050b9b65a567
Miguel Perdomo <filimorniga@GMAIL.COM>
2026-04-19 20:47:20 -0400

    chore: backup logo assets and vector files

--- prosperityapp/dev-dist/sw.js
+++ prosperityapp/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.csknq7ceoq"
+    "revision": "0.9hm0suhfrm8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
@@ -98,7 +98,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
       statuses: [0, 200]
     })]
   }), 'GET');
-  workbox.registerRoute(/^https:\/\/mzoodzsefyaymhjpzopm\.supabase\.co\/(rest|auth)\/.*/i, new workbox.NetworkFirst({
+  workbox.registerRoute(/^https:\/\/mzoodzsefyaymhjpzopm\.supabase\.co\/rest\/.*/i, new workbox.NetworkFirst({
     "cacheName": "supabase-api",
     "networkTimeoutSeconds": 8,
     plugins: [new workbox.ExpirationPlugin({
